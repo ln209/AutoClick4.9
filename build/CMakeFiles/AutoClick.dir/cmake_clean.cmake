a7c7ef39c1e8f51f7133d759fdea970ecc65fb75
ln209 <luis.nahr@outlook.com>
2024-06-19 14:33:47 +0200

    OpenCV_Changes

---
+++ build/CMakeFiles/AutoClick.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "AutoClick.exe"
+  "AutoClick.exe.manifest"
+  "AutoClick.pdb"
+  "CMakeFiles/AutoClick.dir/src/test.cpp.obj"
+  "CMakeFiles/AutoClick.dir/src/test.cpp.obj.d"
+  "libAutoClick.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/AutoClick.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
